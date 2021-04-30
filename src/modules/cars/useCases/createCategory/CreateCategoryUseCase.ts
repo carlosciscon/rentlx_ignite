b0fb73f6e263ff05a023f126a87a51a7d1326412
Carlos Eduardo Ciscon <carlosed@SICOOB3169.LOCAL>
2021-04-30 10:07:10 -0300

    Primeiro commit

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,27 @@
+import {inject, injectable} from "tsyringe";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+@injectable()
+class CreateCategoryUseCase {
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository) {}
+
+    async execute({description, name}: IRequest): Promise<void> {
+        const categoryAlreadExists = await this.categoriesRepository.findByName(name);
+
+        if(categoryAlreadExists){
+            throw new Error("Category already exists!");
+        }
+   
+        this.categoriesRepository.create({name, description});
+    }
+}
+
+export { CreateCategoryUseCase };
