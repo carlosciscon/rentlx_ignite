77c891c5b6b648db4a081ed42e9ff0df34952497
carlosciscon <carlosciscon@gmail.com>
2021-10-06 08:00:55 -0300

    Implementações

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,40 @@
+import { app } from "@shared/infra/http/app";
+import request from "supertest";
+import { hash } from "bcrypt";
+import { v4 as uuid} from uuid;
+
+import createConnection from "@shared/infra/typeorm" 
+import { Connection } from "typeorm";
+
+let connection: Connection;
+
+describe("Create Category Controller", async () => {
+
+    beforeEach( async () => {  
+        connection = await createConnection();
+
+        const id = uuid();
+        const password = await hash("admin", 8);
+
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+            values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+            `    
+        )
+    }) 
+
+    it("Should be able to create a new category", async () => {
+
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com.br",
+            password: "admin",
+        })
+
+        const response = await request(app).post("/categories").send({
+            name: "Category Supertest"   ,
+            description: "Category Supertest"
+        })
+
+        expect(response.status).toBe(201);
+    })
+})
